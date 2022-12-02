cf2ecc768019d7a8c93f15478b01a379befc12c0
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-02 14:43:55 +0100

    BROKERS: Implement Book CRUD Operations

---
+++ JamshidLibrary.App/Brokers/Storages/IStorageBroker.Books.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// -------------------------------------------------------------
+
+using JamshidLibrary.App.Models.Books;
+
+namespace JamshidLibrary.App.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        Book InsertBook(Book book);
+        List<Book> SelectAllBooks();
+        Book SelectBookById(Guid bookId);
+        Book UpdateBook(Book book);
+        Book DeleteBook(Guid book);
+    }
+}
