5802fd60d333c34bf7758e0d4776796eb6b92674
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-03 16:48:08 +0100

    FOUNDATIONS: Initialize BookService

---
+++ JamshidLibrary.App/Services/Foundations/Books/BookService.cs
@@ -0,0 +1,23 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// -------------------------------------------------------------
+
+using JamshidLibrary.App.Brokers.Storages;
+using JamshidLibrary.App.Models.Books;
+
+namespace JamshidLibrary.App.Services.Foundations.Books
+{
+    public class BookService : IBookService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public BookService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public Book AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JamshidLibrary.App/Services/Foundations/Books/IBookService.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// -------------------------------------------------------------
+
+using JamshidLibrary.App.Models.Books;
+
+namespace JamshidLibrary.App.Services.Foundations.Books
+{
+    public interface IBookService
+    {
+        Book AddBook(Book book);
+    }
+}
