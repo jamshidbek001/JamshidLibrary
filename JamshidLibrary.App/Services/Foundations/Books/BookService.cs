09205e4a100d00946ddec3bc8c5fcb54259bc94f
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-03 20:42:14 +0100

    ShouldAddBook -> PASS

--- JamshidLibrary.App/Services/Foundations/Books/BookService.cs
+++ JamshidLibrary.App/Services/Foundations/Books/BookService.cs
@@ -17,7 +17,10 @@ namespace JamshidLibrary.App.Services.Foundations.Books
 
         public Book AddBook(Book book)
         {
-            throw new NotImplementedException();
+            Book storageBook =
+                this.storageBroker.InsertBook(book);
+
+            return storageBook;
         }
     }
 }
